e5321b4cff5f963b7a4f76d5b753c50fda9678a7
nikosajadi <na.sajadii.29@gmail.com>
2024-04-05 01:58:58 +0200

    [feature]first init

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup>
+let fname = "Nasibeh";
+let lfamily = "Sajadi";
+
+
+
+
+
+</script>
+
+<template>
+  <div>
+  <p @class="Header"> my name is {{ fname }}, {{ lfamily }}</p>
+  </div>
+</template>
