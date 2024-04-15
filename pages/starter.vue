c56d18cded545589c27e24aaf677d9fee7df84af
nikosajadi <na.sajadii.29@gmail.com>
2024-04-15 16:16:24 +0200

    [feature] add starter page

--- pages/starter.vue
+++ pages/starter.vue
@@ -1,23 +1,48 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 <template>
-    <div>
-<div class="pageBG w-screen h-screen">
-
-
-    <button class="justify-center absolute w-screen  bottom-5   overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
-        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-        Continiue
-        </span>
-        </button>
-    
-
-
-</div>
+  <div class="pageBG w-screen h-screen flex flex-col items-start ">
+    <div
+      class="w-48 h-1/2 bg-white rounded-b-full flex justify-center self-center  relative items-end"
+    >
+      <div class=" text-sm absolute top-0 text-center mt-5">
+        <p class="FiraCode-SemiBold">Children's social network</p>
+        <p class="FiraCode-Light pt-4 leading-6 p-1">Enrich the sweet world of children with sweet stories</p>
+      </div>
+      <div class="bg-amber-400 w-40 h-40 rounded-full mb-5 flex justify-center ">
+        <img
+          src="/img/books.png"
+          alt="books"
+          class="rounded-full rotate-45 object-contain "
+        />
+      </div>
     </div>
+<div class="absolute bottom-0 w-screen  z-1 self-center">
+    <img src="/img/pattern.png" alt="pattern" class="w-full h-full mix-blend-multiply rotate-180 " />
+</div>
+
+
+    <div class="FiraCode-Light text-white w-[400px] text-lg  text-center self-center mt-20 z-50 ">
+        <p>
+        <span>With</span>
+        <span class="FiraCode-SemiBold mx-3">the world of stories</span>
+        <span>, don't worry about new stories to tell</span>
+        </p>
+      </div>
+
+    <div class="text-white w-screen text-center absolute bottom-5 right-0 z-50 ">
+      <UButton
+        size="lg"
+        class="FiraCode-Medium"
+        color="white"
+        vatiant="soft"
+        label="Continue"
+      />
+    </div>
+  </div>
 </template>
+
 <style>
-.pageBG{
-    background-color: #2BAF66;
+.pageBG {
+  background-color: #2baf66;
 }
 </style>
