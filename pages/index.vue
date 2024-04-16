e08d3dec1e19671663eeb6cba4f5c1812b98ddd2
nikosajadi <na.sajadii.29@gmail.com>
2024-04-16 16:17:01 +0200

    [feature] Use onMounted hook to perform an action

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
+const router = useRouter();
+// Use onMounted hook to perform an action after the component is mounted
+onMounted(async () => { 
+// Execute router navigation after a delay
+setTimeout(() => {
+  router.push({ path: "/starter" });
+}, 40000); // Wait for 2000 milliseconds (2 seconds) before navigating
+      
+    });
 </script>
 <template>
     <div>
 <div class="pageBG w-screen h-screen">
-
+<div>
+    <img src="/public/img/tree.png " alt="tree book" class="w-full h-1/3 mix-blend-multiply "
+</div>
 <div class="text-white absolute w-screen bottom-5 text-center  ">
     <p class="FiraCode-Medium text-sm">The World of story</p>
     <p class="text-sm">StoryLand.se</p>
