8de5dbf038820b681aeaa85dea7d40a3a5482ba3
nikosajadi <na.sajadii.29@gmail.com>
2024-04-18 11:51:18 +0200

    [feature]Add first page

--- pages/index.vue
+++ pages/index.vue
@@ -5,23 +5,25 @@ onMounted(async () => {
 // Execute router navigation after a delay
 setTimeout(() => {
   router.push({ path: "/starter" });
-}, 2000); // Wait for 2000 milliseconds (2 seconds) before navigating
+}, 200000); // Wait for 2000 milliseconds (2 seconds) before navigating
       
     });
 </script>
 <template>
-    <div>
-<div class="pageBG w-screen h-screen">
-<div>
-    <img src="/public/img/tree.png " alt="tree book" class="w-full h-1/3 mix-blend-multiply "
-</div>
+    
+    <div class=" pageBG w-screen h-screen flex flex-col items-start">
+        <div class="h-screen w-full flex flex-col mb-10 justify-center items-center " >
+            <img src="/img/logo.png" alt="logo" class="w-32" >
+            <p class="FiraCode-SemiBold text-2xl text-white pt-10">The world of Story</p>
+        </div>
+
 <div class="text-white absolute w-screen bottom-5 text-center  ">
     <p class="FiraCode-Medium text-sm">The World of story</p>
     <p class="text-sm">StoryLand.se</p>
     <P class="text-xs">V 1.0.2</P>
 </div>
 </div>
-    </div>
+
 </template>
 <style>
 .pageBG{
