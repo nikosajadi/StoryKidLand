ea5b50923e7dbcd32cdc2cbb072a5a3633c415bb
nikosajadi <na.sajadii.29@gmail.com>
2024-04-19 23:56:53 +0200

    [feature] Add Ui

--- pages/intro/login.vue
+++ pages/intro/login.vue
@@ -1,7 +1,7 @@
 <script setup>
 // Initialize router for navigation
 const router = useRouter();
-
+const mobile = ref('')
 // Function to navigate to the next step or route
 const goToNext = () => {
   // Navigate to the "/intro" route
@@ -11,34 +11,38 @@ const goToNext = () => {
 
 <template>
   <div class="w-screen h-screen flex flex-col items-start">
-    <!-- start -->
-    <div
-      class="flex flex-row justify-center w-screen text-center items-center absolute top-10"
-    >
+   <div class="flex flex-row justify-center w-screen text-center items-center absolute top-10">
+     <!-- start -->
+
       <div class="flex flex-col items-center justify-center">
         <p class="FiraCode-SemiBold text-xm text-black">Sign In</p>
         <p class="FiraCode-Medium text-xs text-gray-400 mt-2">
           Get your phone number
         </p>
       </div>
+
     </div>
 
-    <div
-      class="h-screen w-full flex flex-col mb-10 justify-center items-center"
-    >
-      <p class="FiraCode-Medium text-xs text-gray-400 mt-2">
+    <div class="h-screen w-full flex flex-col mb-10 justify-center items-center absolute ">
+      <p class="FiraCode-Medium text-xs text-gray-400 mt-6 ">
         Enter your phone number
       </p>
-      <div>
-        <input type="tel" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
-    </div>
+    
+   <UInput v-model="mobile" class="mt-3 mb-8 w-full px-6" color="gray" variant="outline" size="xl"placeholder="entre your number..." />
 
-      <div class="w-2/3 text-center mt-12">
-      <NuxtLink to="/intro/privacy" class="FiraCode-Medium text-xs text-gray-400 mt-2" >
-          Terms and Conditions
-    </NuxtLink>
+          <!-- Button for navigation to next step -->   
+    <div @click="goToNext" class="FiraCode-Medium flex justify-center text-white w-screen text-center absolute  mt-10 z-50">
+        <div class="bg-green-500 text-white  py-2 mx-5 rounded-lg w-full  text-center tracking-tight">
+          <span>Send Code</span>
+        </div>
       </div>
-    </div>
+
+
+
+      
+      <NuxtLink to="/intro/privacy" class="FiraCode-Medium text-xs text-gray-400  mt-20" > Terms and Conditions </NuxtLink>
+</div>
+    
 
     <!-- Absolute positioned background image -->
     <div class="absolute w-screen top-0 z-1 self-center">
